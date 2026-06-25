e1c8c4acc23d3a0599a37736fa1fa2faa3d74d71
aarnav aakash <energeticboy@aarnavs-MacBook-Air.local>
2026-06-26 02:41:31 +0530

    j

--- check.cpp
+++ check.cpp
@@ -5,3 +5,11 @@
 // Enter initial fuel level (units in tank) [10]: 2
 // Enter fuel purchasing step size (units) [1]: 1.5
 // Enter time penalty for refueling stops (hours) [0.05]: .05
+
+
+
+// cmake -B build -DCMAKE_BUILD_TYPE=Release
+// cmake --build build
+// ./build/fuel_planner
+
+
